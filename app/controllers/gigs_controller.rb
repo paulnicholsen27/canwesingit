f4301d6ba9035640e4bab02558f4e6988de80afc
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-06-13 16:24:01 -0400

    added gig CRUD

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,2 +1,55 @@
 class GigsController < ApplicationController
+
+    before_action :find_gig, only: [:show, :edit, :update, :destroy]
+
+    def new
+        @gig = Gig.new
+    end
+
+    def create
+        @gig = Gig.new(gig_params)
+        if @gig.valid?
+            @gig.save
+            redirect_to @gig
+        else
+            render :new
+        end
+    end
+
+    def index
+        @gigs = Gig.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @gig.update(gig_params)
+        if @gig.valid?
+            @gig.save
+            redirect_to @gig
+        else
+            render :edit
+        end
+
+    end
+
+    def destroy
+        @gig.destroy
+        redirect_to gigs_path
+    end 
+    
+    private
+
+    def find_gig
+        @gig = Gig.find(params[:id])
+    end
+
+    def gig_params
+        params.require(:gig).permit(:name, :notes, :datetime)
+    end
+
 end
