b1711fc456892aafc39f1bdb61b4fd88d7df7e34
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-06-14 12:34:31 -0400

    added primary to singer_roles

--- db/migrate/20190614162300_create_singer_roles.rb
+++ db/migrate/20190614162300_create_singer_roles.rb
@@ -3,7 +3,7 @@ class CreateSingerRoles < ActiveRecord::Migration[5.2]
     create_table :singer_roles do |t|
       t.integer :singer_id
       t.integer :part_id
-
+      t.boolean :primary
       t.timestamps
     end
   end
