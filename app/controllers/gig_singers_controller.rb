97199fb8c98a48ae979c97c0e1fed1def9ee0dba
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-08-01 11:52:51 -0400

    updated checkbox handling on gig/singer

--- app/controllers/gig_singers_controller.rb
+++ app/controllers/gig_singers_controller.rb
@@ -3,11 +3,7 @@ class GigSingersController < ApplicationController
     def create
         gig = Gig.find(params[:gig_id])
         singer_ids = params[:singer_ids] # [1, 4, 5,]
-        gig.singers = []
-        singer_ids.each do |id|
-            singer = Singer.find(id)
-            gig.singers << singer
-        end
+        gig.singers = Singer.find(singer_ids)
         redirect_to gigs_path
     end
 end
